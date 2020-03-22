f465d60de57700634dad7a7f6d9ca2e3f5df2e44
Joao Gilberto Balsini Moura <joaobalsini@gmail.com>
2020-03-22 13:07:45 -0300

    Configure release

---
+++ config/releases.exs
@@ -0,0 +1,6 @@
+import Config
+
+config :mmo_game, MmoGameWeb.Endpoint,
+  server: true,
+  http: [port: {:system, "PORT"}],
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
