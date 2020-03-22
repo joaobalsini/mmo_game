0f03e21df0100001997fad7f95d73a94bfd08271
Joao Gilberto Balsini Moura <joaobalsini@gmail.com>
2020-03-21 22:23:17 -0300

    Added controller actions to show board and heroes, using the api

--- lib/mmo_game_web/controllers/game_controller.ex
+++ lib/mmo_game_web/controllers/game_controller.ex
@@ -1,10 +1,28 @@
 defmodule MmoGameWeb.GameController do
   use MmoGameWeb, :controller
 
-  def index(conn, _params) do
-    with {:ok, grid} <- MmoGame.Grid.default_grid(),
-         {:ok, drawn_grid} <- MmoGame.Grid.draw(grid) do
-      render(conn, "index.html", drawn_grid: drawn_grid, error: nil)
+  def index(conn, %{"name" => hero_name}) do
+    with {:ok, :game_server_started} <- MmoGame.new_with_default_grid(),
+         {:ok, :hero_added} <- MmoGame.add_hero(hero_name),
+         {:ok, drawn_grid} <- MmoGame.draw_grid() do
+      render(conn, "index.html", drawn_grid: drawn_grid, error: nil, hero: hero_name)
+    else
+      {:error, :hero_already_exists} ->
+        # as suggested, both users will be able to control the user
+        {:ok, drawn_grid} = MmoGame.draw_grid()
+        render(conn, "index.html", drawn_grid: drawn_grid, error: nil, hero: hero_name)
+
+      _othee ->
+        render(conn, "index.html", error: "Error drawing the grid")
+    end
+  end
+
+  def index(conn, _other) do
+    with {:ok, :game_server_started} <- MmoGame.new_with_default_grid(),
+         {:ok, hero_name} <- MmoGame.generate_random_name(),
+         {:ok, :hero_added} <- MmoGame.add_hero(hero_name),
+         {:ok, drawn_grid} <- MmoGame.draw_grid() do
+      render(conn, "index.html", drawn_grid: drawn_grid, error: nil, hero: hero_name)
     else
       _any -> render(conn, "index.html", error: "Error drawing the grid")
     end
