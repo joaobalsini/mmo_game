f49b9b9a1c70a6b84b27741ad3c433f336818d44
Joao Gilberto Balsini Moura <joaobalsini@gmail.com>
2020-03-22 21:33:01 -0300

    add page_controller_test

--- test/mmo_game_web/controllers/page_controller_test.exs
+++ test/mmo_game_web/controllers/page_controller_test.exs
@@ -3,6 +3,16 @@ defmodule MmoGameWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Write your name to start"
+  end
+
+  test "Typing the user and clicking start redirects to game", %{conn: conn} do
+    conn = post(conn, Routes.hero_path(conn, :create, %{hero: %{name: "heroname"}}))
+
+    redir_path = redirected_to(conn)
+    assert redir_path == "/game?name=heroname"
+    conn = get(recycle(conn), redir_path)
+
+    assert html_response(conn, 200) =~ "heroname"
   end
 end
