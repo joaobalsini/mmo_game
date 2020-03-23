acab4006af3102ac11d28c408c806ed1c6aac1f5
Joao Gilberto Balsini Moura <joaobalsini@gmail.com>
2020-03-22 21:44:57 -0300

    Added specs to MmoGame

--- lib/mmo_game.ex
+++ lib/mmo_game.ex
@@ -7,18 +7,48 @@ defmodule MmoGame do
   if it comes from the database, an external API or others.
   """
 
+  @spec new_with_default_grid :: {:ok, :game_server_started}
   def new_with_default_grid() do
     with {:ok, grid} <- MmoGame.Grid.default_grid() do
       MmoGame.GameServer.new(grid)
     end
   end
 
+  @spec add_hero(MmoGame.Hero.hero_name()) ::
+          {:error, :game_server_not_started | :hero_already_exists} | {:ok, :hero_added}
   defdelegate add_hero(name), to: MmoGame.GameServer
+
+  @spec remove_hero(MmoGame.Hero.hero_name()) ::
+          {:error, :game_server_not_started | :hero_not_found} | {:ok, :hero_removed}
   defdelegate remove_hero(name), to: MmoGame.GameServer
 
+  @spec move_hero(MmoGame.Hero.hero_name(), MmoGame.Grid.move_direction()) ::
+          {:error,
+           :game_server_not_started
+           | :hero_dead
+           | :hero_not_found
+           | :invalid_move
+           | :invalid_move_parameters}
+          | {:ok, :moved}
   defdelegate move_hero(name, direction), to: MmoGame.GameServer
+
+  @spec attack_from_hero(MmoGame.Hero.hero_name()) ::
+          {:error, :game_server_not_started | :hero_dead | :hero_not_found} | {:ok, :attacked}
   defdelegate attack_from_hero(name), to: MmoGame.GameServer
 
+  @spec draw_grid() ::
+          {:error, :game_server_not_started | :invalid_grid}
+          | {:ok,
+             [
+               [
+                 %{
+                   required(:wall) => boolean(),
+                   optional(MmoGame.Grid.coordinate()) => [
+                     {MmoGame.Hero.hero_name(), :hero_dead | :hero_alive}
+                   ]
+                 }
+               ]
+             ]}
   def draw_grid() do
     with {:ok, grid} <- MmoGame.GameServer.grid(),
          {:ok, heroes_coordinates} <- MmoGame.GameServer.heroes_coordinates() do
@@ -26,5 +56,6 @@ defmodule MmoGame do
     end
   end
 
+  @spec generate_random_name :: {:ok, MmoGame.Hero.hero_name()}
   defdelegate generate_random_name(), to: MmoGame.Utils
 end
